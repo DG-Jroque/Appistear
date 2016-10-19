1786f889a4af54c6d887c05a2f8836894a513fe1
DG-Jroque <byjroke@gmail.com>
2016-10-18 20:51:26 -0500

    Modificacion

--- app/src/main/java/com/example/roque/appistear/Opciones.java
+++ app/src/main/java/com/example/roque/appistear/Opciones.java
@@ -1,6 +1,7 @@
 package com.example.roque.appistear;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,17 @@ public class Opciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
+
+        final Button boton = (Button)findViewById(R.id.btninfo);
+//Implementamos el evento “click” del botón
+        boton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent =  new Intent(Opciones.this, Acercade.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
