f78876e0e83d5d27be385363cf482436dcca4bd6
Carlos <cvc2310@hotmail.com>
2016-10-19 23:13:47 -0500

    efhahg

--- app/src/main/java/com/example/roque/appistear/Opciones.java
+++ app/src/main/java/com/example/roque/appistear/Opciones.java
@@ -29,7 +29,6 @@ public class Opciones extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
-
         //Intent refresh = new Intent(this, MainActivity.class);
         //startActivity(refresh);
         //finish();
