e17968affafb66a321b15f14a7f89c4c14fcd32f
Carlos <cvc2310@hotmail.com>
2016-10-19 22:35:58 -0500

    efhahg

--- app/src/main/java/com/example/roque/appistear/Opciones.java
+++ app/src/main/java/com/example/roque/appistear/Opciones.java
@@ -6,21 +6,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.util.Locale;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 
 public class Opciones extends AppCompatActivity {
+
+
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
-
-        final Button boton = (Button)findViewById(R.id.btninfo);
+        Button btn = (Button)findViewById(R.id.btninfo);
 //Implementamos el evento “click” del botón
-        boton.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 Intent intent =  new Intent(Opciones.this, Acercade.class);
                 startActivity(intent);
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.button_usa);
+//Implementamos el evento “click” del botón
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("en");
+            }
+        });
+
+        Button btn3 = (Button)findViewById(R.id.button_mex);
+//Implementamos el evento “click” del botón
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("es");
             }
         });
 
