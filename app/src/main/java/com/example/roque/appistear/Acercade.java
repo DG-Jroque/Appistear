58549a4de90ab6793f0d009bd9f4be2d678d59d4
DG-Jroque <byjroke@gmail.com>
2016-10-24 18:40:29 -0500

    Modificacion

--- app/src/main/java/com/example/roque/appistear/Acercade.java
+++ app/src/main/java/com/example/roque/appistear/Acercade.java
@@ -1,7 +1,9 @@
 package com.example.roque.appistear;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Acercade extends AppCompatActivity {
 
@@ -9,6 +11,24 @@ public class Acercade extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acercade);
+        Typeface nf = Typeface.createFromAsset(getAssets(),  "fonts/spacecomics.ttf");
+
+        final TextView eq = (TextView) findViewById(R.id.equipo);
+        final TextView cv = (TextView) findViewById(R.id.cv);
+        final TextView rr = (TextView) findViewById(R.id.rr);
+        final TextView ka = (TextView) findViewById(R.id.ka);
+        final TextView ag = (TextView) findViewById(R.id.ag);
+        final TextView jr = (TextView) findViewById(R.id.jr);
+        final TextView p1 = (TextView) findViewById(R.id.p1);
+        final TextView d1 = (TextView) findViewById(R.id.d1);
+        eq.setTypeface(nf);
+        cv.setTypeface(nf);
+        rr.setTypeface(nf);
+        ag.setTypeface(nf);
+        ka.setTypeface(nf);
+        jr.setTypeface(nf);
+        d1.setTypeface(nf);
+        p1.setTypeface(nf);
 
     }
 }
--- app/src/main/java/com/example/roque/appistear/Opciones.java
+++ app/src/main/java/com/example/roque/appistear/Opciones.java
@@ -2,10 +2,14 @@ package com.example.roque.appistear;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Opciones extends AppCompatActivity {
 
@@ -13,9 +17,10 @@ public class Opciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
-
+        Typeface nf = Typeface.createFromAsset(getAssets(),  "fonts/spacecomics.ttf");
         final Button boton = (Button)findViewById(R.id.btninfo);
 //Implementamos el evento “click” del botón
+        boton.setTypeface(nf);
         boton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -23,7 +28,8 @@ public class Opciones extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        final Button btn=(Button)findViewById(R.id.idioma);
+        btn.setTypeface(nf);
 
     }
 }
