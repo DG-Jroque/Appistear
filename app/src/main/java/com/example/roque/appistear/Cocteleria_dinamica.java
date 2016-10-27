d4c86f2455596c78fe7e188e9dbabbf5f4a4bf78
Carlos <cvc2310@hotmail.com>
2016-10-26 23:17:05 -0500

    efhahg

--- app/src/main/java/com/example/roque/appistear/Cocteleria_dinamica.java
+++ app/src/main/java/com/example/roque/appistear/Cocteleria_dinamica.java
@@ -2,12 +2,26 @@ package com.example.roque.appistear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class Cocteleria_dinamica extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocteleria_dinamica);
+        String recibir= getIntent().getStringExtra("var");
+        switch(recibir){
+            case "mararita":
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void margarita(){
+
+
     }
 }
