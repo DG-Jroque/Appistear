af4ff5676edf4583884ba8e0ee825f8f48024abe
Carlos <cvc2310@hotmail.com>
2016-10-27 00:56:17 -0500

    efhahg

--- app/src/main/java/com/example/roque/appistear/Vodka.java
+++ app/src/main/java/com/example/roque/appistear/Vodka.java
@@ -1,7 +1,11 @@
 package com.example.roque.appistear;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Vodka extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class Vodka extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vodka);
+        Typeface nf = Typeface.createFromAsset(getAssets(),  "fonts/spacecomics.ttf");
+
+
+        final Button btn = (Button)findViewById(R.id.buttonWhiteRussian);
+        btn.setTypeface(nf);
+//Implementamos el evento “click” del botón
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent =  new Intent(this, Cocteleria_dinamica.class);
+                intent.putExtra("var", "Vodka_WhiteRussian");
+                startActivity(intent);
+            }
+        });
+
     }
 }
