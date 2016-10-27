0be12b3581b9d876a454c870c062efce8a90fb1e
DG-Jroque <byjroke@gmail.com>
2016-10-26 21:35:51 -0500

    Modificacion

--- app/src/main/java/com/example/roque/appistear/Cocteleria.java
+++ app/src/main/java/com/example/roque/appistear/Cocteleria.java
@@ -1,8 +1,10 @@
 package com.example.roque.appistear;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Cocteleria extends AppCompatActivity {
@@ -17,13 +19,55 @@ public class Cocteleria extends AppCompatActivity {
         final Button bvodka = (Button)findViewById(R.id.btvodka);
         final Button bwhisky = (Button)findViewById(R.id.btwhisky);
         final Button bmezcal = (Button)findViewById(R.id.btmezcal);
-
-//Implementamos el evento “click” del botón
+//Definimos la tipografia
         btequila.setTypeface(nf);
         bron.setTypeface(nf);
         bvodka.setTypeface(nf);
         bwhisky.setTypeface(nf);
         bmezcal.setTypeface(nf);
+
+//Implementamos el evento “click” de los botone
+
+        btequila.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Cocteleria.this,Tequila.class);
+                startActivity(intent);
+            }
+        });
+        bron.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Cocteleria.this,Ron.class);
+                startActivity(intent);
+            }
+        });
+
+        bvodka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Cocteleria.this,Vodka.class);
+                startActivity(intent);
+            }
+        });
+        bwhisky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Cocteleria.this,Whisky.class);
+                startActivity(intent);
+            }
+        });
+
+        bmezcal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Cocteleria.this,Mezcal.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
     }
 }
