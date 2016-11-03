7a6185bbb2fe0a3a9c4ad6ad014f2127433fc8f1
DG-Jroque <byjroke@gmail.com>
2016-11-02 19:05:49 -0600

    Modificacion

---
+++ app/src/main/java/com/example/roque/appistear/JuegoRetos.java
@@ -0,0 +1,29 @@
+package com.example.roque.appistear;
+
+import android.content.res.Resources;
+import android.support.annotation.StringDef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class JuegoRetos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String j="";
+        setContentView(R.layout.activity_juego_retos);
+        TextView tv=(TextView)findViewById(R.id.reto);
+        ArrayList<String> listajugadores = (ArrayList<String>) getIntent().getStringArrayListExtra("arr");
+        ArrayList<String>retos=new ArrayList<String>();
+        int tam=listajugadores.size();
+        int tamret=getResources().getStringArray(R.array.retos).length;
+
+
+        tv.setText(String.valueOf(tam) +" " +String.valueOf(tamret));
+    }
+}
--- app/src/main/java/com/example/roque/appistear/Juegos.java
+++ app/src/main/java/com/example/roque/appistear/Juegos.java
@@ -1,7 +1,11 @@
 package com.example.roque.appistear;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Juegos extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Juegos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juegos);
+        Typeface nf = Typeface.createFromAsset(getAssets(),  "fonts/spacecomics.ttf");
+        Button btretos = (Button)findViewById(R.id.btretos);
+        btretos.setTypeface(nf);
+        btretos.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent =  new Intent(Juegos.this, RetosJugadores.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btcorona=(Button)findViewById(R.id.btcorona);
+        btcorona.setTypeface(nf);
+        btcorona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Juegos.this,CoronaRey.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
