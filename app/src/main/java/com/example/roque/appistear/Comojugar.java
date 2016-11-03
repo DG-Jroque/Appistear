18b5ad5fe1558dbe4e60f25543ad61cd3059be1f
Carlos <cvc2310@hotmail.com>
2016-11-03 09:55:49 -0600

    efhahg

--- app/src/main/java/com/example/roque/appistear/Comojugar.java
+++ app/src/main/java/com/example/roque/appistear/Comojugar.java
@@ -2,6 +2,7 @@ package com.example.roque.appistear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Comojugar extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class Comojugar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comojugar);
         String recibir= getIntent().getStringExtra("var");
+        TextView textView = (TextView)findViewById(R.id.textView12);
+        if(recibir.equalsIgnoreCase("corona")){
+            textView.setText(getString(R.string.copareyReglas));
+        }
+        else{
 
+            ///************REGLAS DE RETOS****************
+        }
     }
 }
