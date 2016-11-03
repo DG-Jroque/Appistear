55e8e30ab177c67e5eea675dc4c8e5130f54b56b
Carlos <cvc2310@hotmail.com>
2016-11-03 01:11:11 -0600

    efhahg

--- app/src/main/java/com/example/roque/appistear/CoronaRey.java
+++ app/src/main/java/com/example/roque/appistear/CoronaRey.java
@@ -1,56 +1,67 @@
 package com.example.roque.appistear;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.renderscript.Allocation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.GestureDetector;
 import android.support.v4.view.GestureDetectorCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class CoronaRey extends AppCompatActivity implements GestureDetector.OnGestureListener,
-GestureDetector.OnDoubleTapListener{
+GestureDetector.OnDoubleTapListener, DatosCopaDelRey{
 
-    private Field []img= new Field[52];
-    private int []Cards = new int [52];
+    private boolean []state= new boolean[52];
+    //private int []Cards = new int [52];
+    public ArrayList<Integer> Cards = new ArrayList<Integer>();
     private GestureDetector gestureDetector;
-    private int pos=-1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corona_rey);
         gestureDetector = new GestureDetector(this, this);
         gestureDetector.setOnDoubleTapListener(this);
-
+        createCards();
+        Button bt= (Button)findViewById(R.id.buttoncoparey);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeBackground();
+            }
+        });
     }
-    public void changeBackground(){
-        pos++;
+    public int changeBackground(){
+        int max= Cards.size();
+        int pos= (int)(Math.random()*max-1);
         RelativeLayout relativeLayout= (RelativeLayout)findViewById(R.id.activity_corona_rey);
-        relativeLayout.setBackgroundResource(Cards[pos]);
-        String name = img[pos].getName();
-        TextView textView = (TextView)findViewById(R.id.textView2);
-        textView.setText(name);
+        relativeLayout.setBackgroundResource(Cards.get(pos));
+        Cards.remove(pos);
+
+         if(Cards.size()==0){
+
+         }
+        return 1;
 
     }
 
     public void createCards(){
-        Field[] ID_Fields = R.mipmap.class.getFields();
-        for(int i = 0; i < ID_Fields.length; i++) {
-            try {
-                if(ID_Fields[i].getName().contains("botella")|| ID_Fields[i].getName().contains("vaso")
-                        || ID_Fields[i].getName().contains("copa") || ID_Fields[i].getName().contains("tarro")){
-                    img[i]= ID_Fields[i];
-                Cards[i] = ID_Fields[i].getInt(null);
-                }
-
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-
-            }
+        for(int i=0; i<image.length;i++){
+            Cards.add(image[i]);
         }
+    }
+
+    public void end(){
+
     }
 
     @Override
