1c5682ffd78b5e80d418befd03093470d2df81d3
Carlos <cvc2310@hotmail.com>
2016-12-12 00:08:42 -0600

    efhahg

--- app/src/main/java/com/DG/roque/appistear/JugadoresDinamicos.java
+++ app/src/main/java/com/DG/roque/appistear/JugadoresDinamicos.java
@@ -54,73 +54,75 @@ public class JugadoresDinamicos extends AppCompatActivity implements InputFilter
         boton2.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                String mng="";
-                names.clear();
-                for(int i=0; i<editTexts.size(); i++) {
-                    if (!editTexts.get(i).getText().toString().trim().equals("") ) {
-                        if(!names.contains(editTexts.get(i).getText().toString().trim())) {
-                            names.add(editTexts.get(i).getText().toString().trim());
-                        }else{
-                            mng= getString(R.string.NombresRepetidos);
-                        }
-
-                    }
-                }
-
-
-                if(names.size()<2){
-                    mng= (names.size()==0) ? getString(R.string.NoNombres)
-                            : getString(R.string.UnNombre);
-                    //ventana alerta que indica que no hay jugadores registrados
-                    AlertDialog.Builder builder = new AlertDialog.Builder(JugadoresDinamicos.this);
-                    builder.setMessage(mng)
-                            .setTitle("Atención!!")
-                            .setCancelable(false)
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .setNeutralButton("Aceptar",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            dialog.cancel();
-                                        }
-                                    });
-                    AlertDialog alert = builder.create();
-                    alert.show();
-                    Button a = alert.getButton(DialogInterface.BUTTON_NEUTRAL);
-                    if(a != null)
-                        a.setBackgroundColor(Color.BLUE);
-                }
-                else{
-                    if(mng== getString(R.string.NombresRepetidos))
-                    {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(JugadoresDinamicos.this);
-                        builder.setMessage(mng)
-                                .setTitle("Atención!!")
-                                .setCancelable(false)
-                                .setIcon(android.R.drawable.ic_dialog_alert)
-                                .setNeutralButton("Aceptar",
-                                        new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialog, int id) {
-                                                dialog.cancel();
-                                            }
-                                        });
-                        AlertDialog alert = builder.create();
-                        alert.show();
-                        Button a = alert.getButton(DialogInterface.BUTTON_NEUTRAL);
-                        if(a != null)
-                            a.setBackgroundColor(Color.BLUE);
-                    }
-                    else
-                    {
-                        Intent intent =  new Intent(JugadoresDinamicos.this, Juegos.class);
-                        intent.putExtra("arr", names);
-                        startActivity(intent);
-                    }
-                }
-
-
+                verifyPlayers();
             }
 
         });
+
+    }
+
+    public void verifyPlayers(){
+        String mng="";
+        names.clear();
+        for(int i=0; i<editTexts.size(); i++) {
+            if (!editTexts.get(i).getText().toString().trim().equals("") ) {
+                if(!names.contains(editTexts.get(i).getText().toString().trim())) {
+                    names.add(editTexts.get(i).getText().toString().trim());
+                }else{
+                    mng= getString(R.string.NombresRepetidos);
+                    editTexts.get(i).requestFocus();
+                }
+
+            }
+        }
+        if(names.size()<2){
+            mng= (names.size()==0) ? getString(R.string.NoNombres)
+                    : getString(R.string.UnNombre);
+            //ventana alerta que indica que no hay jugadores registrados
+            AlertDialog.Builder builder = new AlertDialog.Builder(JugadoresDinamicos.this);
+            builder.setMessage(mng)
+                    .setTitle("Atención!!")
+                    .setCancelable(false)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setNeutralButton("Aceptar",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            AlertDialog alert = builder.create();
+            alert.show();
+            Button a = alert.getButton(DialogInterface.BUTTON_NEUTRAL);
+            if(a != null)
+                a.setBackgroundColor(Color.BLUE);
+        }
+        else{
+            if(mng== getString(R.string.NombresRepetidos))
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(JugadoresDinamicos.this);
+                builder.setMessage(mng)
+                        .setTitle("Atención!!")
+                        .setCancelable(false)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setNeutralButton("Aceptar",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                Button a = alert.getButton(DialogInterface.BUTTON_NEUTRAL);
+                if(a != null)
+                    a.setBackgroundColor(Color.BLUE);
+            }
+            else
+            {
+                Intent intent =  new Intent(JugadoresDinamicos.this, Juegos.class);
+                intent.putExtra("arr", names);
+                startActivity(intent);
+            }
+        }
 
     }
 
@@ -136,14 +138,20 @@ public class JugadoresDinamicos extends AppCompatActivity implements InputFilter
                     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                         // Identifier of the action. This will be either the identifier you supplied,
                         // or EditorInfo.IME_NULL if being called due to the enter key being pressed.
-                        if (actionId == EditorInfo.IME_ACTION_SEARCH
-                                || actionId == EditorInfo.IME_ACTION_DONE
-                                || event.getAction() == KeyEvent.ACTION_DOWN
-                                && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                            createPlayerText();
-                            editTexts.get(editTexts.size()-1).requestFocus();
-                            return true;
+                        try{
+                            if (actionId == EditorInfo.IME_ACTION_SEARCH
+                                    || actionId == EditorInfo.IME_ACTION_DONE
+                                    || event.getAction() == KeyEvent.ACTION_DOWN
+                                    && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                                createPlayerText();
+                                editTexts.get(editTexts.size()-1).requestFocus();
+                                return true;
+                            }
                         }
+                        catch (Exception ex){
+                            verifyPlayers();
+                        }
+
                         // Return true if you have consumed the action, else false.
                         return false;
                     }
